f4a31e9826600f5a33e775a0989e0791dce0a02e
Ennio Simões <ennio.simoes@outlook.com>
2025-08-27 20:18:48 -0300

    add close auction

--- internal/infra/database/auction/create_auction.go
+++ internal/infra/database/auction/create_auction.go
@@ -2,11 +2,16 @@ package auction
 
 import (
 	"context"
+	"fmt"
 	"fullcycle-auction_go/configuration/logger"
 	"fullcycle-auction_go/internal/entity/auction_entity"
 	"fullcycle-auction_go/internal/internal_error"
+	"os"
+	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.uber.org/zap"
 )
 
 type AuctionEntityMongo struct {
@@ -46,5 +51,33 @@ func (ar *AuctionRepository) CreateAuction(
 		return internal_error.NewInternalServerError("Error trying to insert auction")
 	}
 
+	go func() {
+		select {
+		case <-time.After(getAuctionInterval()):
+			update := bson.M{"$set": bson.M{"status": auction_entity.Completed}}
+			filter := bson.M{"_id": auctionEntityMongo.Id}
+
+			_, err := ar.Collection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				logger.Error("Error updating auction status to completed", err)
+				return
+			}
+
+		}
+	}()
+
 	return nil
+}
+
+func getAuctionInterval() time.Duration {
+	auctionInterval := os.Getenv("AUCTION_INTERVAL")
+	fmt.Println("Auction interval:", auctionInterval)
+	duration, err := time.ParseDuration(auctionInterval)
+	if err != nil {
+		logger.Error("Error parsing auction interval", err)
+		return time.Minute * 5
+	}
+
+	fmt.Println("Auction interval set to", zap.String("duration", duration.String()))
+	return duration
 }
